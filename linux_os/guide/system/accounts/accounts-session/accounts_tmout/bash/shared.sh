e5642db177def4bd66d9866213f5cf57cc06b8c1
Israel Villar <israel.villar@ulpgc.es>
2021-09-27 21:41:08 +0100

    Update bash script
    - update to the original repository
    - remove "space" check between variable TMOUT and the equal sign, and between the equal sign and the value
    - Take into account the "readonly" command before the variable.

--- linux_os/guide/system/accounts/accounts-session/accounts_tmout/bash/shared.sh
+++ linux_os/guide/system/accounts/accounts-session/accounts_tmout/bash/shared.sh
@@ -1,10 +1,18 @@
-# platform = Red Hat Enterprise Linux 7,Red Hat Enterprise Linux 8,Red Hat Virtualization 4,multi_platform_fedora,multi_platform_ol,multi_platform_wrlinux
-. /usr/share/scap-security-guide/remediation_functions
-populate var_accounts_tmout
+# platform = multi_platform_all
 
-if grep --silent ^TMOUT /etc/profile ; then
-        sed -i "s/^TMOUT.*/TMOUT=$var_accounts_tmout/g" /etc/profile
-else
-        echo -e "\n# Set TMOUT to $var_accounts_tmout per security requirements" >> /etc/profile
-        echo "TMOUT=$var_accounts_tmout" >> /etc/profile
+{{{ bash_instantiate_variables("var_accounts_tmout") }}}
+
+# if 0, no occurence of tmout found, if 1, occurence found
+tmout_found=0
+
+for f in /etc/profile /etc/profile.d/*.sh; do
+    if grep --silent -E '(readonly)?\s*TMOUT' $f; then
+        sed -i -E 's/TMOUT=[[:digit:]]+(.*)$/TMOUT='$var_accounts_tmout'\1/g' $f
+        tmout_found=1
+    fi
+done
+
+if [ $tmout_found -eq 0 ]; then
+        echo -e "\n# Set TMOUT to $var_accounts_tmout per security requirements" >> /etc/profile.d/tmout.sh
+        echo "readonly TMOUT=$var_accounts_tmout" >> /etc/profile.d/tmout.sh
 fi
